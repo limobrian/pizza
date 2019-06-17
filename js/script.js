cb19408d83e6e5bd246450cf2ab9e40d9fb2ab95
briankiplimo <limobrian290@gmail.com>
2019-06-17 22:31:22 +0300

    added js functions

--- js/script.js
+++ js/script.js
@@ -2,19 +2,24 @@ function update() {
   var size = document.getElementById("size");
   var topping = document.getElementById("topping");
   var delivery = document.getElementById("delivery");
+  var crust=document.getElementById("crust");
   var priceSize = size.options[size.selectedIndex].value;
   var itemSize = size.options[size.selectedIndex].text;
   var priceTopping = topping.options[topping.selectedIndex].value;
   var itemTopping = topping.options[topping.selectedIndex].text;
   var priceDelivery = delivery.options[delivery.selectedIndex].value;
   var itemDelivery = delivery.options[delivery.selectedIndex].text;
-  var total = parseInt(priceSize) + parseInt(priceDelivery) + parseInt(priceTopping);
+  var pricecrust = crust.options[crust.selectedIndex].value;
+  var itemcrust = crust.options[crust.selectedIndex].text;
+  var total = parseInt(priceSize) + parseInt(priceDelivery) + parseInt(priceTopping) + parseInt(pricecrust);
   $(".size1").html(itemSize);
   $(".size2").html(priceSize);
   $(".topping1").html(itemTopping);
   $(".topping2").html(priceTopping);
   $(".delivery1").html(itemDelivery);
   $(".delivery2").html(priceDelivery);
+  $(".crust1").html(itemDelivery);
+  $(".crust2").html(priceDelivery);
   $(".total1").html("Total");
   $(".total2").html(total);
 }
