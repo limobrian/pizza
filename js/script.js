759a8a05e710ad5fbbcbd747070c377d5ae72cf6
briankiplimo <limobrian290@gmail.com>
2019-06-18 04:53:52 +0300

    js on submistion

--- js/script.js
+++ js/script.js
@@ -23,3 +23,18 @@ function update() {
   $(".total1").html("Total");
   $(".total2").html(total);
 }
+$("#submission").submit(function(event){
+  var data_1 = $("input:first").val();
+  var data_2 = $("input#mail").val();
+  if(data_1 && data_2 != ""  ){
+    $(".message").text("Thank you " + data_1 + " Your message has been received");
+    $(".message").removeClass("error");
+    $(".message").addClass("success");
+  }else{
+    $(".message").text("Error!!! Please fill in all the details correctly");
+    $(".message").addClass("error");
+  }
+  event.preventDefault();
+});
+
+});
