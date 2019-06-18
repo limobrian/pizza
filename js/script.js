9839c34b9daa76e96cb980680abbf94561239c23
briankiplimo <limobrian290@gmail.com>
2019-06-18 04:57:21 +0300

    delivery method

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function update() {
   var size = document.getElementById("size");
   var topping = document.getElementById("topping");
   var delivery = document.getElementById("delivery");
-  var crust=document.getElementById("crust");
+  var crust = document.getElementById("crust");
   var priceSize = size.options[size.selectedIndex].value;
   var itemSize = size.options[size.selectedIndex].text;
   var priceTopping = topping.options[topping.selectedIndex].value;
@@ -23,14 +23,15 @@ function update() {
   $(".total1").html("Total");
   $(".total2").html(total);
 }
-$("#submission").submit(function(event){
+$(document).ready(function () {
+$("#submission").submit(function (event) {
   var data_1 = $("input:first").val();
   var data_2 = $("input#mail").val();
-  if(data_1 && data_2 != ""  ){
+  if (data_1 && data_2 != "") {
     $(".message").text("Thank you " + data_1 + " Your message has been received");
     $(".message").removeClass("error");
     $(".message").addClass("success");
-  }else{
+  } else {
     $(".message").text("Error!!! Please fill in all the details correctly");
     $(".message").addClass("error");
   }
